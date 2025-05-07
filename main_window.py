5e64e7acd4498a192d704e12c3b23485a96a0d04
drus454 <ljllhl@yandex.ru>
2025-05-07 17:20:30 +0300

    -

--- main_window.py
+++
@@ -1,7 +0,0 @@
-from PySide6 import QtWidgets
-
-class MainWindow(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Основное окно')
-        self.resize(300, 200)
